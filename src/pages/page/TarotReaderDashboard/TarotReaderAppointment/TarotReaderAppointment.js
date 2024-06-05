c5f89ded29d3858e7953d0da772e187df8e2cf77
TinNTGaming <84866171+TinNTGaming@users.noreply.github.com>
2024-06-06 04:12:08 +0700

    GetAll

--- src/pages/page/TarotReaderDashboard/TarotReaderAppointment/TarotReaderAppointment.js
+++ src/pages/page/TarotReaderDashboard/TarotReaderAppointment/TarotReaderAppointment.js
@@ -1,15 +1,42 @@
 import "./TarotReaderAppointment.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function TarotReaderAppointment() {
+    const [appointments, setAppointments] = useState([]);
+    const id = 2;
+
+    useEffect(() => {
+        const fetchAppointments = async () => {
+            try {
+                const response = await axios.get("https://localhost:7218/api/Bookings");
+                const formattedAppointments = response.data.map(appointment => {
+                    const formattedDate = new Date(appointment.date).toLocaleDateString('en-GB');
+                    const formattedTime = new Date(appointment.startTime).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' });
+                    return { ...appointment, date: formattedDate, startTime: formattedTime };
+                });
+    
+                // Tìm appointment có tarotReaderId tương ứng với id
+                const filteredAppointments = formattedAppointments.filter(appointment => appointment.tarotReaderId === id);
+    
+                setAppointments(filteredAppointments);
+            } catch (error) {
+                console.error("Error fetching appointments", error);
+            }
+        };
+    
+        fetchAppointments();
+    }, [id]);
+
     return (  
         <div>
-            <div class="row">
+            <div className="row">
                 <h1 className="title">Lịch hẹn</h1>
             </div>
-            <div class="row">
-                <div class="col-12 mt-4">
-                    <div class="table-responsive bg-white shadow rounded">
-                        <table class="table mb-0 table-center">
+            <div className="row">
+                <div className="col-12 mt-4">
+                    <div className="table-responsive bg-white shadow rounded">
+                        <table className="table mb-0 table-center">
                             <thead>
                                 <tr>
                                     <th className="border-bottom p-3" style={{ minWidth: '50px' }}>#</th>
@@ -25,37 +52,29 @@ function TarotReaderAppointment() {
                             </thead>
 
                             <tbody>
-                                <tr>
-                                    <th class="p-3">1</th>
-                                    <td class="p-3">Nguyen Van B</td>
-                                    <td class="p-3">25</td>
-                                    <td class="p-3">Male</td>
-                                    <td class="p-3">012345678</td>
-                                    <td class="p-3">20th Dec 2020</td>
-                                    <td class="p-3">11:00AM</td>                                
-                                    <td class="p-3">$50</td>
-                                    <td class="text-end p-3">
-                                        {/* <a href="#" class="btn btn-icon btn-pills btn-soft-primary" data-bs-toggle="modal" data-bs-target="#viewappointment"><i class="uil uil-eye"></i></a>
-                                        <a href="#" class="btn btn-icon btn-pills btn-soft-success" data-bs-toggle="modal" data-bs-target="#acceptappointment"><i class="uil uil-check-circle"></i></a>
-                                        <a href="#" class="btn btn-icon btn-pills btn-soft-danger" data-bs-toggle="modal" data-bs-target="#cancelappointment"><i class="uil uil-times-circle"></i></a> */}
-                                    </td>
-                                </tr>
-                                
-                                <tr>
-                                    <th class="p-3">2</th>
-                                    <td class="p-3">Nguyen Van A</td>
-                                    <td class="p-3">28</td>
-                                    <td class="p-3">Female</td>
-                                    <td class="p-3">012345679</td>
-                                    <td class="p-3">20th Dec 2020</td>
-                                    <td class="p-3">11:00AM</td>
-                                    <td class="p-3">$50</td>
-                                    <td class="text-end p-3">
-                                        {/* <a href="#" class="btn btn-icon btn-pills btn-soft-primary" data-bs-toggle="modal" data-bs-target="#viewappointment"><i class="uil uil-eye"></i></a>
-                                        <a href="#" class="btn btn-icon btn-pills btn-soft-success" data-bs-toggle="modal" data-bs-target="#acceptappointment"><i class="uil uil-check-circle"></i></a>
-                                        <a href="#" class="btn btn-icon btn-pills btn-soft-danger" data-bs-toggle="modal" data-bs-target="#cancelappointment"><i class="uil uil-times-circle"></i></a> */}
-                                    </td>
-                                </tr>
+                                {appointments.map((appointment, index) => (
+                                    <tr key={index}>
+                                        <th className="p-3">{index + 1}</th>
+                                        <td className="p-3">{appointment.customerName}</td>
+                                        <td className="p-3">{appointment.age}</td>
+                                        <td className="p-3">{appointment.gender ? "Nữ" : "Nam"}</td>
+                                        <td className="p-3">{appointment.phoneNumber}</td>
+                                        <td className="p-3">{appointment.date}</td>
+                                        <td className="p-3">{appointment.startTime}</td>
+                                        <td className="p-3">{appointment.sessionTypeName}</td>
+                                        {appointment.status === undefined ? (
+                                            <td className="p-3">
+                                                <button className="btn btn-success">Xác nhận</button>
+                                                <button className="btn btn-danger">Hủy</button>
+                                            </td>
+                                        ):(
+                                            <td className={`p-3 ${appointment.status === 'Confirmed' ? 'confirmed' : 'disabled'}`}>                                                                                        
+                                                {appointment.status === 'Confirmed' ? 'Xác nhận' : ''}
+                                                {appointment.status === 'Disabled' ? 'Hủy' : ''}
+                                            </td>
+                                        )}
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
